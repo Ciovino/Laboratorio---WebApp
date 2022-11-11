90b72e59e6b155401467cbaec00aa628d198ccd1
Ciovino <tarantino.giovanbattista01@gmail.com>
2022-11-11 19:20:01 +0100

    post tramite dizionario su python

--- app.py
+++ app.py
@@ -7,7 +7,17 @@ app = Flask(__name__)
 # Define route and web pages
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    posts = [{'fotoPost':'Immagini/img1.jpg', 'fotoProfilo':'Immagini/Profilo.png', 'username':'luigi', 'giorniFa':2, 
+                'contenuto':'''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis enim ut egestas lacinia. 
+                Vestibulum sodales at odio non semper. Donec mattis pellentesque rutrum.'''},
+            {'fotoPost':'Immagini/img2.jpg', 'fotoProfilo':'Immagini/Profilo.png', 'username':'alberto', 'giorniFa':4,
+                'contenuto':'''Nullam nunc eros, sagittis eu gravida vel, interdum a tellus. Praesent quis lectus semper, 
+                interdum mauris eu, molestie ligula. Morbi sagittis ullamcorper blandit.'''},
+            {'fotoPost':'Immagini/img3.jpg', 'fotoProfilo':'Immagini/Profilo.png', 'username':'juan', 'giorniFa':4, 
+                'contenuto':'''Vestibulum quis lectus nec odio mollis consectetur vel non libero. In ac leo dolor. Nunc 
+                consequat quam et leo pretium porta.'''}]
+
+    return render_template('homepage.html', posts=posts)
 
 @app.route('/presentazione.html')
 def presentazione():
