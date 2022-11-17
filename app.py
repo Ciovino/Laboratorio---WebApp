5fa4c1c2b1cc718dac7f9632c72a9f4b796c6561
Ciovino <tarantino.giovanbattista01@gmail.com>
2022-11-17 18:01:24 +0100

    route dinamica per i singoli post

--- app.py
+++ app.py
@@ -4,21 +4,26 @@ from flask import Flask, render_template
 # Create the application
 app = Flask(__name__)
 
+posts = [{'id': 1, 'fotoPost':'Immagini/img1.jpg', 'fotoProfilo':'Immagini/Profilo.png', 'username':'luigi', 'giorniFa':2, 
+            'contenuto':'''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis enim ut egestas lacinia. 
+            Vestibulum sodales at odio non semper. Donec mattis pellentesque rutrum.'''},
+        {'id': 2, 'fotoPost':'Immagini/img2.jpg', 'fotoProfilo':'Immagini/Profilo.png', 'username':'alberto', 'giorniFa':4,
+            'contenuto':'''Nullam nunc eros, sagittis eu gravida vel, interdum a tellus. Praesent quis lectus semper, 
+            interdum mauris eu, molestie ligula. Morbi sagittis ullamcorper blandit.'''},
+        {'id': 3, 'fotoPost':'Immagini/img3.jpg', 'fotoProfilo':'Immagini/Profilo.png', 'username':'juan', 'giorniFa':4, 
+            'contenuto':'''Vestibulum quis lectus nec odio mollis consectetur vel non libero. In ac leo dolor. Nunc 
+            consequat quam et leo pretium porta.'''}]
+
 # Define route and web pages
 @app.route('/')
 def homepage():
-    posts = [{'fotoPost':'Immagini/img1.jpg', 'fotoProfilo':'Immagini/Profilo.png', 'username':'luigi', 'giorniFa':2, 
-                'contenuto':'''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis enim ut egestas lacinia. 
-                Vestibulum sodales at odio non semper. Donec mattis pellentesque rutrum.'''},
-            {'fotoPost':'Immagini/img2.jpg', 'fotoProfilo':'Immagini/Profilo.png', 'username':'alberto', 'giorniFa':4,
-                'contenuto':'''Nullam nunc eros, sagittis eu gravida vel, interdum a tellus. Praesent quis lectus semper, 
-                interdum mauris eu, molestie ligula. Morbi sagittis ullamcorper blandit.'''},
-            {'fotoPost':'Immagini/img3.jpg', 'fotoProfilo':'Immagini/Profilo.png', 'username':'juan', 'giorniFa':4, 
-                'contenuto':'''Vestibulum quis lectus nec odio mollis consectetur vel non libero. In ac leo dolor. Nunc 
-                consequat quam et leo pretium porta.'''}]
-
     return render_template('homepage.html', posts=posts)
 
 @app.route('/presentazione.html')
 def presentazione():
     return render_template('presentazione.html')
+
+@app.route('/posts/<int:id>')
+def post(id):
+    post = [p for p in posts if p['id'] == id][0]
+    return render_template('post.html', post=post)
